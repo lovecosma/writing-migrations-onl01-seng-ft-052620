b44b0318b6cfa169c4614af0b1dca5d17aa68cc2
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-27 00:49:17 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,8 +1,5 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :students do |t|
-      t.integer :grade
-      t.string :birthdate
-    end
+    add_column :grade, :birthdate
   end
 end
