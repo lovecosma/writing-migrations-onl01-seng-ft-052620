85a1730b3b5de40211fe7ed0d7d61b95d4c471c2
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-27 00:27:57 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -18,3 +18,10 @@ def drop_db
     DB.execute("DROP TABLE #{table}")
   end
 end
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
+ 
+require_relative "../student.rb"
